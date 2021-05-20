5fa937c40fbc13a573cb903d4dc3305f51f7289e
oManhattan <80975790+oManhattan@users.noreply.github.com>
2021-05-20 19:16:44 -0300

    Add files via upload

--- Templo 03_(Conversão de bases)/Implementacao_Templo.java
+++ Templo 03_(Conversão de bases)/Implementacao_Templo.java
@@ -110,7 +110,7 @@ public class Implementacao_Templo {
         //Fazer processo de desafio com opcoes
         Timer();
         System.out.printf("\n***\tTexto para começar o primeiro desafio\t***\nConverter o numero %S que está na base %d para a base %d\n\n", numeroInicial, baseInicial, baseConversao);
-        
+
         System.out.printf("a) %S\nb) %S\nc) %S\nd) %S\ne) %S\n\n", opcoes[0], opcoes[1], opcoes[2], opcoes[3], opcoes[4]);
 
         System.out.println("Resposta certa: " + respostaCerta.toUpperCase());
@@ -342,11 +342,164 @@ public class Implementacao_Templo {
     }
 
     static int[] Desafio3(int healthPoints, int qntPotions) {
+        //Local para iniciar as variaveis
+
         int[] status = new int[2];
         status[0] = healthPoints;
         status[1] = qntPotions;
 
+        int[] auxConv = new int[4];
+
+        String[] opcoes = new String[5];
+
+        ArrayList<String> respostaErrada = new ArrayList();
+
+        boolean condicao = true;
+        int baseInicial, baseConversao;
+        String multiplicadorInicialA, multiplicadorInicialB, divisorInicial, respostaCerta, respostaPlayer = null;
+
+        Scanner input = new Scanner(System.in);
+        //**************************************************************************
+        //Gerar a base inicial
+        baseInicial = DeterminaBase();
+
+        //Gerar a base para conversao
+        do {
+            baseConversao = DeterminaBase();
+        } while (baseConversao == baseInicial);
+
+        //Gerar 3 numeros para a base inicial
+        multiplicadorInicialA = DeterminaNumero(baseInicial);
+        multiplicadorInicialB = DeterminaNumero(baseInicial);
+        divisorInicial = DeterminaNumero(baseInicial);
+
+        //Realizar a conta e fazer a conversao dos valores para gerar uma resposta
+        auxConv[0] = Integer.parseInt(multiplicadorInicialA, baseInicial);
+        auxConv[1] = Integer.parseInt(multiplicadorInicialB, baseInicial);
+        auxConv[2] = Integer.parseInt(divisorInicial, baseInicial);
+        auxConv[3] = (auxConv[0] * auxConv[1]);
+        auxConv[3] = auxConv[3] / auxConv[2];
+
+        respostaCerta = Integer.toString(auxConv[3], baseConversao);
+
+        //Gerar as outras opcoes para o desafio
+        respostaErrada.add(respostaCerta);
+
+        for (int i = 0; i <= 3; i++) {
+            respostaErrada.add(DeterminaOpcoes(auxConv[3], baseConversao, respostaCerta));
+        }
+
+        Collections.shuffle(respostaErrada);
+        Collections.shuffle(respostaErrada);
+
+        for (int i = 0; i < opcoes.length; i++) {
+            opcoes[i] = respostaErrada.get(i);
+        }
+
+        //**************************************************************************
+        //Iniciar com o texto e a parte visual do desafio
+        System.out.println("***\tÚLTIMO DESAFIO DO TERCEIRO TEMPLO\t***\n");
+        Timer();
+        System.out.printf("Faça a seguinte a seguinte conta com os numeros na base %d e depois converta para a base %d\n", baseInicial, baseConversao);
+        Timer();
+        System.out.printf("\t%S     *     %S\n\t--------------------\n\t\t%S\n\n", multiplicadorInicialA, multiplicadorInicialB, divisorInicial);
+        Timer();
+        System.out.printf("a) %S\n", opcoes[0]);
+        Timer2();
+        System.out.printf("b) %S\n", opcoes[1]);
+        Timer2();
+        System.out.printf("c) %S\n", opcoes[2]);
+        Timer2();
+        System.out.printf("d) %S\n", opcoes[3]);
+        Timer2();
+        System.out.printf("e) %S\n\n", opcoes[4]);
+        Timer2();
+        System.out.println("Resposta certa: \n" + respostaCerta);
+        Timer2();
+
+        //Iniciar parte que o Player interage
+        do {
+            try {
+                System.out.print("Escolha uma opção: ");
+                respostaPlayer = input.next();
+                respostaPlayer = respostaPlayer.toUpperCase();
+                System.out.println(" ");
+            } catch (Exception e) {
+                System.out.println("Digite um caracter válido.\nOu algo deu errado com o codigo");
+            }
+
+            switch (respostaPlayer.charAt(0)) {
+                case 'A':
+                    if (opcoes[0].equals(respostaCerta)) {
+                        AcertouResposta(status[0]);
+                        condicao = false;
+                    } else {
+                        status[0] = DanoRespostaErrada(status[0]);
+                        if (status[0] <= 0) {
+                            GameOver();
+                            condicao = false;
+                        }
+                    }
+                    break;
+                case 'B':
+                    if (opcoes[1].equals(respostaCerta)) {
+                        AcertouResposta(status[0]);
+                        condicao = false;
+                    } else {
+                        status[0] = DanoRespostaErrada(status[0]);
+                        if (status[0] <= 0) {
+                            GameOver();
+                            condicao = false;
+                        }
+                    }
+                    break;
+                case 'C':
+                    if (opcoes[2].equals(respostaCerta)) {
+                        AcertouResposta(status[0]);
+                        condicao = false;
+                    } else {
+                        status[0] = DanoRespostaErrada(status[0]);
+                        if (status[0] <= 0) {
+                            GameOver();
+                            condicao = false;
+                        }
+                    }
+                    break;
+                case 'D':
+                    if (opcoes[3].equals(respostaCerta)) {
+                        AcertouResposta(status[0]);
+                        condicao = false;
+                    } else {
+                        status[0] = DanoRespostaErrada(status[0]);
+                        if (status[0] <= 0) {
+                            GameOver();
+                            condicao = false;
+                        }
+                    }
+                    break;
+                case 'E':
+                    if (opcoes[4].equals(respostaCerta)) {
+                        AcertouResposta(status[0]);
+                        condicao = false;
+                    } else {
+                        status[0] = DanoRespostaErrada(status[0]);
+                        if (status[0] <= 0) {
+                            GameOver();
+                            condicao = false;
+                        }
+                    }
+                    break;
+                case 'P':
+                    int[] aux = UsePotion(status[0], status[1]);
+
+                    status[0] = aux[0];
+                    status[1] = aux[1];
+            }
+
+        } while (condicao);
+
         return status;
+
     }
 
     //**************************************************************************
